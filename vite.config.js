817c4a2defbb513a81a459d3d7876a87f20408a6
James Colestock <james@colestock.com>
2025-09-27 19:11:53 +0000

    fix(vite): config forced / base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/',
+  base: command === "serve" ? "/" : undefined, // use --base flag for builds
   build: {
     outDir: "dist",
   },
-});
+}));
